2e35d7fd334f8cfb1204e27067ac34ed36448ec0
Your Name <you@example.com>
2019-05-09 22:36:30 +0300

    # WARNING: head commit changed in the meantime
    add commidity

---
+++ base_enh/models/commodity.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+
+class Commodity(models.Model):
+    _name = 'commodity'
+    
+    name = fields.Char ('Name')
+    temperature = fields.Float('Temperature')
+    warehouse_condition = fields.Char('Warehouse condition')
+    warehouse_condition_att = fields.Binary(attachment=True,string="Attachment")
+    transport_condition = fields.Char('Transport condition')
+    transport_condition_att = fields.Binary(attachment=True,string="Attachment")
+    port_condition = fields.Char('Port condition')
+    port_condition_att = fields.Binary(attachment=True,string="Attachment")
+    other_condition = fields.Char('Other condition')
+    other_condition_att = fields.Binary(attachment=True,string="Attachment")
+    commodity_category = fields.Many2one('commodity', string='Commodity Category')
+    
