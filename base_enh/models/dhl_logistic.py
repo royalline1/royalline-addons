e8cdb6a703b325a25f55ab9a8e8dbad54163a457
badi home <you@example.com>
2019-06-27 16:31:43 +0300

    [enhancement for DHL logistic views]

--- base_enh/models/dhl_logistic.py
+++ base_enh/models/dhl_logistic.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import Warning
 
 class DHLLogistic(models.Model):
     _name='dhl.logistic'
@@ -8,7 +9,7 @@ class DHLLogistic(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     
 #   Shipment from fields  
-    company_from_id = fields.Many2one('res.partner', string='Company Name')
+    company_from_id = fields.Many2one('res.partner', string='Contact Name')
     date_from = fields.Datetime('Date Time Pick-Up')
     country_from_id = fields.Many2one('res.country', string="Country", related='company_from_id.country_id')
     state_from_id = fields.Many2one('res.country.state', string="State", related='company_from_id.state_id')
@@ -19,18 +20,19 @@ class DHLLogistic(models.Model):
     contact_person_from_ids = fields.One2many(comodel_name='res.partner',inverse_name='dhl_log_id', 
                                               related='company_from_id.child_ids')
     
-    company_to_id = fields.Many2one('res.partner', string='Company Name')
+    company_to_id = fields.Many2one('res.partner', string='Contact Name')
     country_to_id = fields.Many2one('res.country', string="Country", related='company_to_id.country_id')
     state_to_id = fields.Many2one('res.country.state', string="State", related='company_to_id.state_id')
     city_to_id = fields.Many2one('res.city', string="City", related='company_to_id.city_id')
     phone_to = fields.Char(string='Phone', related='company_to_id.phone')
     add_one_to = fields.Char(string='Address (1)', related='company_to_id.street_name')
     add_two_to = fields.Char(string='Address (2)', related='company_to_id.street2')
+    space_btwn = fields.Char(string='     ')
     contact_person_to_ids = fields.One2many(comodel_name='res.partner',inverse_name='dhl_log_to_id', 
                                               related='company_to_id.child_ids')
     document_type_ids = fields.One2many(comodel_name='document.type', inverse_name='dhl_doc_id')
     
-
+                
 class DocumentType(models.Model):
     _name='document.type'
     _description = "Document Type"
